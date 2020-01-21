59c57821ad83cd4846b3085615fd2225d95f18ae
MegaGrindStone <coinrubik@gmail.com>
2020-01-21 20:56:58 +0700

    Change parameter

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -289,6 +289,7 @@ public:
 
         genesis = CreateGenesisBlock(1390095618, 28917698, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
+        printf("hashMerkleRoot %s\n", genesis.hashMerkleRoot.ToString().c_str());
         assert(consensus.hashGenesisBlock == uint256S("0x00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6"));
         assert(genesis.hashMerkleRoot == uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
 
@@ -875,7 +876,7 @@ CChainParams& Params(const std::string& chain)
         assert(devNetParams);
         return *devNetParams;
     } else if (chain == CBaseChainParams::REGTEST)
-        return regTestParams;
+            return regTestParams;
     else
         throw std::runtime_error(strprintf("%s: Unknown chain %s.", __func__, chain));
 }
