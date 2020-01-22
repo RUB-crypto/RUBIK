0877fe52e54eebcf4dcbb3e38e3e776905aed8eb
MegaGrindStone <coinrubik@gmail.com>
2020-01-22 18:35:53 +0700

    Remove seeds

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -8,6 +8,9 @@
  * IPv4 as well as onion addresses are wrapped inside a IPv6 address accordingly.
  */
 static SeedSpec6 pnSeed6_main[] = {
+};
+
+static SeedSpec6 pnSeed6_test[] = {
     
 };
 #endif // RUBIK_CHAINPARAMSSEEDS_H
